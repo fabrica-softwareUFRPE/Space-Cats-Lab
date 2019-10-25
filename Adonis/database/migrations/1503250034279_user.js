65494116e08178386a9038c4d14c0932fc937904
igor-felipe <54617151+igor-felipe@users.noreply.github.com>
2019-10-25 16:47:53 -0300

    Refactor: add attributes in user migration
    Adicão de alguns atributos e modificadores na migration de user.

--- Adonis/database/migrations/1503250034279_user.js
+++ Adonis/database/migrations/1503250034279_user.js
@@ -6,10 +6,23 @@ const Schema = use('Schema')
 class UserSchema extends Schema {
   up () {
     this.create('users', (table) => {
+      
       table.increments()
+      
       table.string('username', 80).notNullable().unique()
+      
       table.string('email', 254).notNullable().unique()
+      
       table.string('password', 60).notNullable()
+      
+      table.string('cpf', 11).notNullable().unique().primary() // adiciona o CPF - que é o ID do user - Chave primária.
+      
+      table.integer('nivel').notNullable().defaultTo(1) // O nível do usuário (1, 2 ou 3)
+      
+      // setor é chave estrangeira. 1000 provavelmente é muito.
+      // referencia id (do setor) na tabela userSetor
+      table.string('userSetor', 1000).references('id').inTable(userSetor).onUpdate("CASCADE"); 
+      
       table.timestamps()
     })
   }
