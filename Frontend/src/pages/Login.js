6813c3e276ca431d4e676a6ecf2801e5aae7f804
Geraldo Fernando <gfernandopaes@gmail.com>
2019-10-26 13:36:59 -0300

    Create React-App usado pra fazer a pasta frontend com as dependências, src movido para lá.

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -10,83 +10,50 @@ import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import InputLabel from '@material-ui/core/InputLabel';
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+
 function Login() {
-
-
-
     return (
-
-
-        
         //div que contém todos os elemento que serão retornados na chamada da função.
         <div className="login-container">
+            <grid>
+                <form>
+                    <div className="logo">
+                        <img src={logo} alt="Logo" />
+                    </div>
 
+                    <div className="inputs">
+                        <Grid container spacing={1} alignItems="flex-end">
+                            <Grid item>
+                                <AccountCircleOutlinedIcon />
+                            </Grid>
+                            <Grid item>
+                                <TextField id="input-login" label="Digite seu Login" margin="normal"
 
-<grid>
-            <form>
-
-                <div className="logo">
-
-                    <img src={logo} alt="Logo" />
-
-                </div>
-
-
-                <div className="inputs">
-
-
-                    <Grid container spacing={1} alignItems="flex-end">
-                        <Grid item>
-                            <AccountCircleOutlinedIcon />
+                                />
+                            </Grid>
                         </Grid>
-                        <Grid item>
-                            <TextField id="input-login" label="Digite seu Login" margin="normal"
 
-                            />
+                        <Grid container spacing={1} alignItems="flex-end">
+                            <Grid item>
+                                <LockOutlinedIcon />
+                            </Grid>
+                            <Grid item>
+
+                                <TextField type="password" id="input-senha" label="Digite sua senha"
+                                    margin="normal" />
+
+                            </Grid>
                         </Grid>
-                    </Grid>
+                    </div>
 
-                    <Grid container spacing={1} alignItems="flex-end">
-                        <Grid item>
-                            <LockOutlinedIcon />
-                        </Grid>
-                        <Grid item>
+                    <div className="buttons">
 
-                            <TextField type="password" id="input-senha" label="Digite sua senha"
-                                margin="normal" />
-
-                        </Grid>
-                    </Grid>
-
-                </div>
-
-                <div className="buttons">
-
-                    <button type="submit">Entrar</button>
-
-
-                </div>
-
-
-
-            </form>
-
-
-</grid>
-
-   
-
-
-         
+                        <button type="submit">Entrar</button>
+                    </div>
+                </form>
+            </grid>
         </div>
-
-
-
-
-
     )
-
-
 }
 
 export default Login;
