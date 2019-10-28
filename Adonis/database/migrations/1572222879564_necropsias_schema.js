98394a078e273c53ce6917b701c88169192f9b7c
Matheus Mosca <matheusmoscag@gmail.com>
2019-10-27 23:02:05 -0300

    Refactor: Tabela de Necropsias

---
+++ Adonis/database/migrations/1572222879564_necropsias_schema.js
@@ -0,0 +1,29 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class NecropsiasSchema extends Schema {
+  up () {
+    this.create('necropsias', (table) => {
+      table.increments()
+      table.string('data').notNullable()
+      table.string('ficha', 11).notNullable() // número identificador do animal
+      table.string('nome', 80).notNullable()
+      table.string('especie', 80).notNullable()
+      table.string('peso').notNullable() // peso até 100kg?
+      table
+        .integer('setor_id')
+        .unique()
+        .notNullable()
+        //.references('id')
+        //.inTable('sectors')
+    })
+  }
+
+  down () {
+    this.drop('necropsias')
+  }
+}
+
+module.exports = NecropsiasSchema
