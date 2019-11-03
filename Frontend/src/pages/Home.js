bcb2810b6aafa478136e1f0211fc20bf75f5f0c7
Geraldo Fernando <gfernandopaes@gmail.com>
2019-11-03 12:45:11 -0300

    Consertando algumas warnings e mudanças na Home
    Agora a sidebar da home tem três divs, uma pra o nome do sistema e do usuário (e possivelmente um botão como o do mockup, se precisar), uma no meio pra os botões de setores e uma no fim pra o botão de sair. Também comentei algumas coisas que estavam causando warnings ao rodar o front.

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -6,6 +6,10 @@ export default function Home() {
     return(
         <div className="pageHome">
             <div className="sideBar">
+                <div className="headDiv">
+                    <p><center>Sistema FORDHOV<br/>Hospital Veterinário UFRPE</center></p>
+                    <p>USERNAME</p>
+                </div>
                 <div className="naviDiv">
                     <p>Hello System FORDHOV</p>
                     <p>Hello System FORDHOV</p>
--- Frontend/src/pages/LoginTest.js
+++ Frontend/src/pages/LoginTest.js
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 import './LoginTest.css';
 import logo from '../assets/logo.png'
 import TextField from '@material-ui/core/TextField';
-import IconButton from '@material-ui/core/IconButton';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import AccountCircle from '@material-ui/icons/AccountCircle';
+//import IconButton from '@material-ui/core/IconButton';
+//import InputAdornment from '@material-ui/core/InputAdornment';
+//import AccountCircle from '@material-ui/icons/AccountCircle';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import InputLabel from '@material-ui/core/InputLabel';
+//import InputLabel from '@material-ui/core/InputLabel';
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
 
 export default function Login() {
