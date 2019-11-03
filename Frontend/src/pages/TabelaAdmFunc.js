f6f11b6f18916d9b0fefc55e3026785682fb3e39
eduardo-diniz <eduardogomesdiniz42@gmail.com>
2019-11-03 20:26:20 -0300

    Tabela_ADM_func V 0.5

--- Frontend/src/pages/TabelaAdmFunc.js
+++ Frontend/src/pages/TabelaAdmFunc.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import MaterialTable from 'material-table';
 import './TabelaAdmFunc.css';
+import Button from '@material-ui/core/Button';
+
+
 
 export default function TabelaFuncionario() {
   const [state, setState] = React.useState({
@@ -17,7 +20,6 @@ export default function TabelaFuncionario() {
     ],
     data: [
       { nome: 'Eduardo 0', login: 'PCEdu42', senha: 123456789, email: 'Eduardogomes42@hotmail.com', funcao: 1 },
-
       { nome: 'Eduardo 1', login: 'PCEdu42', senha: 123456789, email: 'Eduardogomes42@hotmail.com', funcao: 1 },
       { nome: 'Eduardo 2', login: 'PCEdu42', senha: 123456789, email: 'Eduardogomes42@hotmail.com', funcao: 1 },
       { nome: 'Eduardo 3', login: 'PCEdu42', senha: 123456789, email: 'Eduardogomes42@hotmail.com', funcao: 1 },
@@ -32,12 +34,79 @@ export default function TabelaFuncionario() {
     ],
   });
 
+  
   return (
-    <MaterialTable
+
+
+
+
+<div className = 'tela-adm'  >
+
+  
+  <link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+/>
+
+      <div className="sideBar">
+                <div className="headDiv">
+                    <p><center>SAC-FORDHOV<br/>Hospital Veterinário UFRPE</center></p>
+                    <p>USERNAME</p>
+                </div>
+                <div className="naviDiv">
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Gerenciar Usuários</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Consultas e Retornos</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Diagnóstico por Imagem</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Procedimentos Cirúrgicos</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Procedimentos Anestésicos</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Análises Laboratoriais</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Necropsias</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Atendimentos Externos</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Imprimir Relatório Mensal</button>
+                    <div className="naviLine"/>
+                </div>
+                <div className="exitDiv">
+                    <button id="exitButton">Sair</button>
+                </div>
+            </div>
+
+ 
+
+    <MaterialTable 
       title="Funcionarios"
       columns={state.columns}
       data={state.data}
-      editable={{
+      actions={[
+        {
+          icon: 'save',
+          tooltip: 'Save User',
+          onClick: (event, rowData) => alert("You saved " + rowData.name)
+        }
+      ]}
+    
+      components={{
+        Action: props => (
+          <Button
+            onClick={(event) => props.action.onClick(event, props.data)}
+            color="primary"
+            variant="contained"
+            style={{textTransform: 'none'}}
+            size="small"
+          >
+            My Button
+          </Button>
+        ),
+      }}
+     /* editable={{
         onRowAdd: newData =>
           new Promise(resolve => {
             setTimeout(() => {
@@ -48,32 +117,12 @@ export default function TabelaFuncionario() {
                 return { ...prevState, data };
               });
             }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-                setState(prevState => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return { ...prevState, data };
-                });
-              }
-            }, 600);
-          }),
-        onRowDelete: oldData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              setState(prevState => {
-                const data = [...prevState.data];
-                data.splice(data.indexOf(oldData), 1);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-      }}
+          })}}
+    */
+      
     />
+    </div>
   );
 }
+
+      
