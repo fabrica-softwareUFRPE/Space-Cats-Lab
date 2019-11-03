b5d740820fb9a6b7a92619b45a712d42c8c14075
Geraldo Fernando <gfernandopaes@gmail.com>
2019-11-03 17:37:33 -0300

    Botões adicionados à Home.
    Coloquei uma série de botões na barra lateral, que vão servir pra direcionar o usuário pras outras páginas. Ainda preciso ver como vou implementar as cortinas pra abrir as subdivisões de cada um, se não não vai caber tudo.

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -7,15 +7,29 @@ export default function Home() {
         <div className="pageHome">
             <div className="sideBar">
                 <div className="headDiv">
-                    <p><center>Sistema FORDHOV<br/>Hospital Veterinário UFRPE</center></p>
+                    <p><center>SAC-FORDHOV<br/>Hospital Veterinário UFRPE</center></p>
                     <p>USERNAME</p>
                 </div>
                 <div className="naviDiv">
-                    <p>Hello System FORDHOV</p>
-                    <p>Hello System FORDHOV</p>
-                    <p>Hello System FORDHOV</p>
-                    <p>Hello System FORDHOV</p>
-                    <p>Hello System FORDHOV</p>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Gerenciar Usuários</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Consultas e Retornos</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Diagnóstico por Imagem</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Procedimentos Cirúrgicos</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Procedimentos Anestésicos</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Análises Laboratoriais</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Necropsias</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Atendimentos Externos</button>
+                    <div className="naviLine"/>
+                    <button className="naviButton" id="naviButton1">Imprimir Relatório Mensal</button>
+                    <div className="naviLine"/>
                 </div>
                 <div className="exitDiv">
                     <button id="exitButton">Sair</button>
