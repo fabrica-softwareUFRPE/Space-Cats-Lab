9c9c5c99295eaaa44b1562e3be0139cb232738ce
unknown <rtibirica82@gmail.com>
2019-11-12 22:22:50 -0300

    atualizacao da tabela v1.2.3

--- Frontend/src/pages/TabelaAdmFunc.js
+++ Frontend/src/pages/TabelaAdmFunc.js
@@ -33,93 +33,96 @@ export default function TabelaFuncionario() {
            
     ],
   });
-
   
   return (
 
 
 
 
-<div className = 'tela-adm'  >
+    <div className = 'tela-adm'  >
 
-  
-  <link
-  rel="stylesheet"
-  href="https://fonts.googleapis.com/icon?family=Material+Icons"
-/>
+      
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
 
       <div className="sideBar">
-                <div className="headDiv">
-                    <p><center>SAC-FORDHOV<br/>Hospital Veterinário UFRPE</center></p>
-                    <p>USERNAME</p>
-                </div>
-                <div className="naviDiv">
-                    <div className="naviLine"/>
-                    <button className="naviButton" id="naviButton1">Gerenciar Usuários</button>
-                    <div className="naviLine"/>
-                    <button className="naviButton" id="naviButton1">Consultas e Retornos</button>
-                    <div className="naviLine"/>
-                    <button className="naviButton" id="naviButton1">Diagnóstico por Imagem</button>
-                    <div className="naviLine"/>
-                    <button className="naviButton" id="naviButton1">Procedimentos Cirúrgicos</button>
-                    <div className="naviLine"/>
-                    <button className="naviButton" id="naviButton1">Procedimentos Anestésicos</button>
-                    <div className="naviLine"/>
-                    <button className="naviButton" id="naviButton1">Análises Laboratoriais</button>
-                    <div className="naviLine"/>
-                    <button className="naviButton" id="naviButton1">Necropsias</button>
-                    <div className="naviLine"/>
-                    <button className="naviButton" id="naviButton1">Atendimentos Externos</button>
-                    <div className="naviLine"/>
-                    <button className="naviButton" id="naviButton1">Imprimir Relatório Mensal</button>
-                    <div className="naviLine"/>
-                </div>
-                <div className="exitDiv">
-                    <button id="exitButton">Sair</button>
-                </div>
-            </div>
+        <div className="headDiv">
+            <p><center>SAC-FORDHOV<br/>Hospital Veterinário UFRPE</center></p>
+            <p>USERNAME</p>
+        </div>
+        <div className="naviDiv">
+            <div className="naviLine"/>
+            <button className="naviButton" id="naviButton1">Gerenciar Usuários</button>
+            <div className="naviLine"/>
+            <button className="naviButton" id="naviButton1">Consultas e Retornos</button>
+            <div className="naviLine"/>
+            <button className="naviButton" id="naviButton1">Diagnóstico por Imagem</button>
+            <div className="naviLine"/>
+            <button className="naviButton" id="naviButton1">Procedimentos Cirúrgicos</button>
+            <div className="naviLine"/>
+            <button className="naviButton" id="naviButton1">Procedimentos Anestésicos</button>
+            <div className="naviLine"/>
+            <button className="naviButton" id="naviButton1">Análises Laboratoriais</button>
+            <div className="naviLine"/>
+            <button className="naviButton" id="naviButton1">Necropsias</button>
+            <div className="naviLine"/>
+            <button className="naviButton" id="naviButton1">Atendimentos Externos</button>
+            <div className="naviLine"/>
+            <button className="naviButton" id="naviButton1">Imprimir Relatório Mensal</button>
+            <div className="naviLine"/>
+        </div>
+        <div className="exitDiv">
+            <button id="exitButton">Sair</button>
+        </div>
+      </div>
 
- 
 
-    <MaterialTable 
-      title="Funcionarios"
-      options={{
-        rowStyle: {
-          height: 10,
-          width:  1,
-          //backgroundColor: '#fff',
-        }
-      }}
-      columns={state.columns}
-      data={state.data}
-      editable={{
-        onRowAdd: newData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              setState(prevState => {
-                const data = [...prevState.data];
-                data.push(newData);
-                return { ...prevState, data };
-              });
-            }, 600);
-          }),
-        onRowUpdate: (newData, oldData) =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              if (oldData) {
-                setState(prevState => {
-                  const data = [...prevState.data];
-                  data[data.indexOf(oldData)] = newData;
-                  return { ...prevState, data };
-                });
+      <div className="page-content" >
+        <div className="table-wrapper" >
+          <MaterialTable 
+            title="Funcionarios"
+            options={{
+              rowStyle: {
+                height: 10,
+                width:  1,
+                //backgroundColor: '#fff',
               }
-            }, 600);
-          })}}
-  
+            }}
+            columns={state.columns}
+            data={state.data}
+            editable={{
+              onRowAdd: newData =>
+                new Promise(resolve => {
+                  setTimeout(() => {
+                    resolve();
+                    setState(prevState => {
+                      const data = [...prevState.data];
+                      data.push(newData);
+                      return { ...prevState, data };
+                    });
+                  }, 600);
+                }),
+              onRowUpdate: (newData, oldData) =>
+                new Promise(resolve => {
+                  setTimeout(() => {
+                    resolve();
+                    if (oldData) {
+                      setState(prevState => {
+                        const data = [...prevState.data];
+                        data[data.indexOf(oldData)] = newData;
+                        return { ...prevState, data };
+                      });
+                    }
+                  }, 600);
+                })}}
+
+            
+          />
+        </div>
+      </div>
       
-    />
     </div>
   );
 }
