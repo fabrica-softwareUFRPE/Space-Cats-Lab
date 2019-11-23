c86942967ddd90949698a1d445e85d11dee85e0d
Space Cats Lab <54604987+fabrica-softwareUFRPE@users.noreply.github.com>
2019-11-22 23:56:09 -0300

    Update Routes

--- Frontend/src/routes.js
+++ Frontend/src/routes.js
@@ -6,6 +6,9 @@ import TabelaFuncionario from './pages/TabelaAdmFunc';
 import Home from './pages/Home';
 import ManageUsers from './pages/TableGerenciarUsersTest';
 import TabelaPlanilhaAdm from './pages/TabelaP.Anestésicos P. Porte';
+import TabelaConsultaPeq from './pages/TabelaPConsultaRetornoPeq';
+import TabelaConsultaGra from './pages/TabelaPConsultaRetornoGra';
+import TabelaConsultaSil from './pages/TabelaPConsultaRetornoSil';
 
 export default function Routes(){
     return (
@@ -15,6 +18,9 @@ export default function Routes(){
             <Route path = "/table" component = {TabelaFuncionario} />
             <Route path = "/tableusers" component = {ManageUsers} />
             <Route path = "/tableplan" component = {TabelaPlanilhaAdm} />
+            <Route path = "/tableConsultaPeq" component = {TabelaConsultaPeq}/>
+            <Route path = "/tableConsultaGra" component = {TabelaConsultaGra}/>
+            <Route path = "/tableConsultaSil" component = {TabelaConsultaSil}/>
         </BrowserRouter>
     );
 }
